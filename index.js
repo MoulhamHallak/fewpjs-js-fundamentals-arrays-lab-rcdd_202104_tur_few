9bb4a8557c8f88e6a6067eb572f8680f76a68cb6
Moulham Hallak <mou990@gmail.com>
2021-04-22 21:20:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const prepend = ["Milo", "Otis", "Garfield"];
 prepend.unshift("odie");
 
 const removeLast = ["Milo", "Otis", "Garfield"];
-removeLast.pop;
+removeLast.pop();
 
 const removeFirst = ["Milo", "Otis", "Garfield"];
-removeFirst.shift;
+removeFirst.shift();
