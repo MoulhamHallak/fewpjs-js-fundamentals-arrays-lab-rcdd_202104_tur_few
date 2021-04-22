96095b0426b37b2667a6e2a1cb9c2b12773573a3
Moulham Hallak <mou990@gmail.com>
2021-04-22 21:22:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"];
-append.push("odie");
+append.push("Odie");
 
 const prepend = ["Milo", "Otis", "Garfield"];
-prepend.unshift("odie");
+prepend.unshift("Odie");
 
 const removeLast = ["Milo", "Otis", "Garfield"];
 removeLast.pop();
